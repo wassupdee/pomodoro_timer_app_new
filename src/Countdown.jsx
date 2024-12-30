9d817f1e478790baf0ad57cff27c95d2ffce9097
wassupdee <daichi.hirohata.japan@gmail.com>
2024-12-30 19:21:35 +0900

    fix: timerのIDをuseRefを使用しグローバルスコープで管理する

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useRef } from 'react';
 
 const Countdown = () => {
 
   //作業時間（25分）からカウントダウンを始める
   const [remainingTimeMs, setRemainingTimeMs] = useState(25 * 60 * 1000);
+  const timerRef = useRef(0);
 
   const startTimer = () => {
-    const timer = setInterval(() => {
+    const timerId = setInterval(() => {
       setRemainingTimeMs((prev) => prev - 1000)
     }, 1000);
+    timerRef.current = timerId;
   };
 
   const finish_sound = new Audio("/finish_whistle.wav");
