d3f04e65bd81d3fb7c4b4e50cd7fd8b8e6f26352
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-09 14:02:21 +0900

    feat: ログアウトコンポーネントをルート内に入れて表示する（別途、カウントダウンと一緒に表示するように変更予定）

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthProvider from './components/AuthProvider';
 import Countdown from './components/Countdown';
 import SignIn from './components/SignIn';
+import SignOut from './components/SignOut';
 // import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Countdown />} />
             <Route path="/signin" element={<SignIn />} />
+            <Route path="/signout" element={<SignOut />} />
           </Routes>
         </Router>
       </AuthProvider>
