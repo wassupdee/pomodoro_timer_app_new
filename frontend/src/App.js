93c511ce93d8d5d463df41c3e90f8c4229120d5d
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-09 13:23:59 +0900

    feat: ルートパスにカウントダウンを、/signupパスにサインアップモジュールを割り当て、全体をAuthProviderでネストし、認証機能を提供

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import Countdown from './Countdown.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthProvider from './components/AuthProvider';
+import Countdown from './components/Countdown';
+import SignIn from './components/SignIn';
+// import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <Countdown />
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Countdown />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
-}
+};
 
 export default App;
