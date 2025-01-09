287034ae935880a8daa7881787b77898d6882164
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-09 13:18:29 +0900

    feat: サインインページを担当するコンポーネントを作成

---
+++ frontend/src/components/SignIn.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "./AuthProvider";
+
+// サインインページを担当する（サインイン機能はAuthProviderから受けとる）
+export const SignIn = () => {
+  const { signin } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignInSubmit = async (e) => {
+    e.preventDefault();
+    const success = await signin(email, password);
+
+    if (success) {
+      // サインイン成功時の通知、及びリダイレクトは別途実装する
+      console.log("Sign-in success");
+    } else {
+      // サインイン失敗時の通知は別途実装する
+      console.log("Sign-in failed");
+    }
+  };
+
+  return (
+    <>
+      <p>サインインページです</p>
+      <form>
+        <div>
+          <label htmlFor="email">メールアドレス</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" onClick={(e) => handleSignInSubmit(e)}>
+          Submit
+        </button>
+      </form>
+      <Link to="/signup">サインアップへ</Link>
+    </>
+  );
+};
+
+export default SignIn;
