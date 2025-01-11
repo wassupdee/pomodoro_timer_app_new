fa65edd820060481a93d9d16e1c78d2e18352c22
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-11 14:47:56 +0900

    feat: 作業・休憩時間を保存するコントローラーV1::TimerRecordsControllerを作成

---
+++ backend/app/controllers/v1/timer_records_controller.rb
@@ -0,0 +1,33 @@
+class V1::TimerRecordsController < ApplicationController
+  before_action :authenticate_v1_user!
+
+  def create_or_update
+    # 今日の日付を取得
+    today = Time.zone.today
+
+    today_timer_record = current_v1_user.timer_records.find_by(recorded_date: today)
+
+    if today_timer_record
+      # 日付が一致するレコードがあれば更新する
+      if today_timer_record.update(timer_record_params)
+        render json: { message: '計測時間レコードが更新されました', data: today_timer_record }
+      else
+        render json: { message: '計測時間レコードの更新に失敗しました', errors: today_timer_record.errors.full_messages }
+      end
+    else
+      # 日付が一致するレコードがなければ新しくつくる
+      new_timer_record = current_v1_user.timer_records.new(timer_record_params.merge(recorded_date: today))
+      if new_timer_record.save
+        render json: { message: '新しい計測時間レコードが保存されました' }
+      else
+        render json: { message: '新しい計測時間レコードの保存に失敗しました', errors: new_timer_record.errors.full_messages }
+      end
+    end
+  end
+
+  private
+
+  def timer_record_params
+    params.permit(:study_time_ms, :rest_time_ms)
+  end
+end
