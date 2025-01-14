5a90ec6f81fd430592a4fa76933529face26c4d1
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-14 15:39:56 +0900

    feat: パラメーターで受けとる作業または休憩時間を、DBに保存されている作業または休憩時間に加算する

--- backend/app/controllers/v1/timer_records_controller.rb
+++ backend/app/controllers/v1/timer_records_controller.rb
@@ -1,21 +1,16 @@
 class V1::TimerRecordsController < ApplicationController
   before_action :authenticate_v1_user!
+  before_action :find_today_timer_record, only: [:create_or_update]
 
   def create_or_update
-    # 今日の日付を取得
-    today = Time.zone.today
-
-    today_timer_record = current_v1_user.timer_records.find_by(recorded_date: today)
-
-    if today_timer_record
-      # 日付が一致するレコードがあれば更新する
-      if today_timer_record.update(timer_record_params)
-        render json: { message: '計測時間レコードが更新されました', data: today_timer_record }
+    # 日付が一致するレコードがあれば更新する。なければ、新しくレコードをつくる
+    if @today_timer_record
+      if @today_timer_record.update(merged_time_elapsed_ms(@today_timer_record))
+        render json: { message: '計測時間レコードが更新されました', data: @today_timer_record }
       else
-        render json: { message: '計測時間レコードの更新に失敗しました', errors: today_timer_record.errors.full_messages }
+        render json: { message: '計測時間レコードの更新に失敗しました', errors: @today_timer_record.errors.full_messages }
       end
     else
-      # 日付が一致するレコードがなければ新しくつくる
       new_timer_record = current_v1_user.timer_records.new(timer_record_params.merge(recorded_date: today))
       if new_timer_record.save
         render json: { message: '新しい計測時間レコードが保存されました' }
@@ -29,5 +24,16 @@ class V1::TimerRecordsController < ApplicationController
 
   def timer_record_params
     params.permit(:work_time_elapsed_ms, :rest_time_elapsed_ms)
+  end
+
+  def merged_time_elapsed_ms(timer_record)
+    {
+      work_time_elapsed_ms: timer_record.work_time_elapsed_ms.to_i + timer_record_params[:work_time_elapsed_ms].to_i,
+      rest_time_elapsed_ms: timer_record.rest_time_elapsed_ms.to_i + timer_record_params[:rest_time_elapsed_ms].to_i
+    }
+  end
+
+  def find_today_timer_record
+    @today_timer_record = current_v1_user.timer_records.find_by(recorded_date: Time.zone.today)
   end
 end
