82a3a42cb7cdc8d6f008f99ab7f1e8aad6c6ddb1
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-15 18:22:18 +0900

    chore: TypeScriptファイルに変更し、型を定義

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,13 +1,16 @@
 import applyCaseMiddleware from 'axios-case-converter'
-import axios from 'axios'
-
+import axios, { AxiosInstance } from 'axios';
 
 // API通信を行うためのクライアント設定
-const options = {
+interface Options {
+    ignoreHeaders: boolean;
+}
+
+const options: Options = {
     ignoreHeaders: true,
 }
 
-const client = applyCaseMiddleware(
+const client: AxiosInstance = applyCaseMiddleware(
     axios.create({
         baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
     }),
