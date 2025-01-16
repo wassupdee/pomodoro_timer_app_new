8ca1841596b9effafb186f5b85d0d61aff0be3d1
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-16 12:16:51 +0900

    refactor: axiosのリクエストの設定情報をconfigとして定義（configの中のheadersだけを定義していたが、拡張性を考えより包括的な名前へ変更）

--- frontend/src/api/saveTimerRecord.ts
+++ frontend/src/api/saveTimerRecord.ts
@@ -8,17 +8,19 @@ const saveTimerRecord = async (workTimeElapsedMs: number, restTimeElapsedMs: num
     restTimeElapsedMs,
   };
 
-  const headers: AxiosRequestConfig["headers"] = {
-    "access-token": Cookies.get("_access_token") || "",
-    client: Cookies.get("_client") || "",
-    uid: Cookies.get("_uid") || "",
+  const config: AxiosRequestConfig = {
+    headers: {
+      "access-token": Cookies.get("_access_token") || "",
+      client: Cookies.get("_client") || "",
+      uid: Cookies.get("_uid") || "",
+    }
   };
 
   try {
     const res = await client.post(
       "v1/timer_records",
       params,
-      { headers }
+      config,
     );
     if (res.status === 200) {
       return true;
