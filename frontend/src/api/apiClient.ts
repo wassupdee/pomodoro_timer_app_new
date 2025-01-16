78a8b88bbb88b08cfd64150198ef07683e97b575
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-16 11:54:42 +0900

    fix: axiosのリクエストオプションに、ignoreheadersプロパティは存在しないため、これを削除

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,19 +2,10 @@ import applyCaseMiddleware from 'axios-case-converter'
 import axios, { AxiosInstance } from 'axios';
 
 // API通信を行うためのクライアント設定
-interface Options {
-    ignoreHeaders: boolean;
-}
-
-const options: Options = {
-    ignoreHeaders: true,
-}
-
 const client: AxiosInstance = applyCaseMiddleware(
     axios.create({
         baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
     }),
-    options
 );
 
 export default client;
