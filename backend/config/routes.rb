f6ff8dc8f1c074aecc6bdbe4ad2f53787472eb36
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-17 19:30:16 +0900

    chore: 開発環境でブラウザでメールを確認できるようにするため、letter opener webをインストール

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :v1 do
     # 認証機能
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
