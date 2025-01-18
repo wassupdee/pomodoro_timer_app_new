8acebaa4c56a43fe7c225f6c9ec957df3179c61f
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-18 11:23:14 +0900

    fix/refactor: gmailのsmtpとのタイムアウトエラーが起きた為、open timeoutとread timeoutを５→１０秒に変更。エラーの出力を出すように設定。環境変数を使用

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -34,22 +34,24 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
+  config.action_mailer.default_options = { from: ENV.fetch("EMAIL_ADDRESS") }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_SERVER_HOST") }
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: ENV['EMAIL_ADDRESS'],
-    password: ENV['EMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
+    user_name: ENV.fetch("EMAIL_ADDRESS"),
+    password: ENV.fetch("EMAIL_PASSWORD"),
+    authentication: :plain,
+    enable_starttls_auto: true,
+    open_timeout: 10, # ← 追加
+    read_timeout: 10  # ← 追加
   }
 
   # Print deprecation notices to the Rails logger.
