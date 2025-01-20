1859c19f4ff1b911a69f8369041d64ffc6c35962
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-20 11:34:37 +0900

    chore: DBとのセッションが残っているので、DBのdropコマンドを削除

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -11,5 +11,5 @@ bundle install
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
-# bundle exec rails db:migrate
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
+bundle exec rails db:migrate
