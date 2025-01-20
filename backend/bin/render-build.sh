b3ebccb881d388da650def9a4d4a1cb069c8a1b8
wassupdee <daichi.hirohata.japan@gmail.com>
2025-01-20 11:03:43 +0900

    chore: 本番環境のDBをリセットするために、スクリプトを変更（55fa60aでタイポがあったので修正）

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -11,5 +11,5 @@ bundle install
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-bundle exec rails db:migrate
+bundle exec rails db:reset
 # bundle exec rails db:migrate
